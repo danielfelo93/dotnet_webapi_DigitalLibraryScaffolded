876f10064244b3521a309151e8d3781a9cb5d1fa
Daniel Londoño <danielfelo93@gmail.com>
2024-10-24 13:51:08 -0500

    Restored deleted files from branch LibreriaApp

---
+++ LibreriaDigitalApp/LIBRERIADIGITAL.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIBRERIADIGITAL.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController() {
+        
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> GetHelloWorld()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ LibreriaDigitalApp/LIBRERIADIGITAL.WebApi/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LIBRERIADIGITAL.WebApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        //protected override void Onmodelcreating(Modelbuilder modelbuilder)
+        //{
+        //    modelbuilder.entity<user>().totable("users");
+        //    modelbuilder.entity<book>().totable("books");
+        //}
+    }
+}
